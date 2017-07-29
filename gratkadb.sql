bbf1c73bed91f2802faae8d2c942cb085a613872
Ola <al.bednarczyk@gmail.com>
2017-07-29 10:33:25 +0200

    Database update

--- gratkadb.sql
+++ gratkadb.sql
@@ -5,12 +5,37 @@ create table students(
 id_student int primary key auto_increment not null,
 first_name varchar(15) not null,
 last_name varchar(15) not null
-
 );
 
-drop table students;
 insert into students values (1, 'Jan', 'Nowak');
 insert into students values (2, 'Anna ', 'Kowalska');
 insert into students values (3, 'Piotr', 'Kowalski');
+
+create table questions(
+id_question int primary key auto_increment not null,
+question_body text not null,
+option_a text not null,
+option_b text not null,
+option_c text not null,
+option_d text not null,
+answear varchar(1) not null,
+category varchar(3) not null
+);
+
+insert into questions values (1, 'Treść pytania 1', 'odpowiedz a', 'odpowiedz b', 'odpowiedz c','odpowiedz d', 'c', 'J');
+insert into questions values (2, 'Treść pytania 2', 'odpowiedz a', 'odpowiedz b', 'odpowiedz c','odpowiedz d', 'c', 'P');
+
+create table results(
+id_result int primary key auto_increment not null,
+student int not null,
+result int not null,
+foreign key(student) references students(id_student) 
+);
+
+insert into results values (1, 2, 80);
+insert into results values (2, 2, 60);
+insert into results values (3, 3, 55);
+
+
 
 
